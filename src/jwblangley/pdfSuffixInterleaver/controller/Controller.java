903ce5e128e36c0760abc67e48da98ea4ebb6fde
James Langley <jwblangley@gmail.com>
2020-03-25 18:38:30 +0000

    Complete implementation: connect controller

--- src/jwblangley/pdfSuffixInterleaver/controller/Controller.java
+++ src/jwblangley/pdfSuffixInterleaver/controller/Controller.java
@@ -1,15 +1,51 @@
 package jwblangley.pdfSuffixInterleaver.controller;
 
+import java.io.File;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import jwblangley.pdfSuffixInterleaver.model.PDFSuffixInterleaver;
 import jwblangley.pdfSuffixInterleaver.view.ViewLayout;
 
 public class Controller extends Application {
 
+  private ViewLayout layout;
+
+  private File documentPdf;
+  private File suffixPdf;
+
+  public void setDocumentPdf(File documentPdf) {
+    this.documentPdf = documentPdf;
+  }
+
+  public void setSuffixPdf(File suffixPdf) {
+    this.suffixPdf = suffixPdf;
+  }
+
+  public void runInterleaver() {
+    if (suffixPdf == null) {
+      layout.reportStatus("Please select a suffix pdf", false);
+      return;
+    }
+    if (documentPdf == null) {
+      layout.reportStatus("Please select a document pdf", false);
+      return;
+    }
+    try {
+      layout.reportStatus("Working...", true);
+      PDFSuffixInterleaver.interleaveSuffixSeparateAndSave(suffixPdf, documentPdf);
+      layout.reportStatus("Process complete", true);
+    } catch (IOException e) {
+      e.printStackTrace();
+      layout.reportStatus("An error occurred", false);
+    }
+  }
+
   @Override
   public void start(Stage stage) throws Exception {
-    Scene scene = new Scene(ViewLayout.layout(stage));
+    layout = new ViewLayout(this);
+    Scene scene = new Scene(layout.layout(stage));
 
     stage.setTitle("PDF Suffix Interleaver");
     stage.setScene(scene);
--- src/jwblangley/pdfSuffixInterleaver/view/ViewLayout.java
+++ src/jwblangley/pdfSuffixInterleaver/view/ViewLayout.java
@@ -1,30 +1,95 @@
 package jwblangley.pdfSuffixInterleaver.view;
 
+import java.io.File;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import jwblangley.pdfSuffixInterleaver.controller.Controller;
 
 public class ViewLayout {
 
-  private static final int WIDTH = 500;
-  private static final int HEIGHT = 200;
+  private static final int WIDTH = 600;
+  private static final int HEIGHT = 150;
 
-  public static Pane layout(Stage window) {
-    HBox layout = new HBox(5);
-    layout.setPrefSize(WIDTH, HEIGHT);
+  private final Controller controller;
 
-    Button selectSuffixButton = new Button("Select suffix file");
-    selectSuffixButton.setPrefSize(WIDTH / 2, HEIGHT);
-    selectSuffixButton.setOnAction(e -> System.out.println("select suffix"));
+  private Button selectSuffixButton;
+  private Button selectDocumentButton;
 
-    Button selectDocumentButton = new Button("Select document file");
-    selectDocumentButton.setPrefSize(WIDTH / 2, HEIGHT);
-    selectDocumentButton.setOnAction(e -> System.out.println("select document"));
+  private Label statusLabel;
 
-    layout.getChildren().addAll(selectSuffixButton, selectDocumentButton);
+  public ViewLayout(Controller controller) {
+    this.controller = controller;
+  }
 
-    return layout;
+  public Pane layout(Stage window) {
+    Font font = new Font(18);
+
+    BorderPane rootNode = new BorderPane();
+
+    HBox buttonLayout = new HBox(5);
+    buttonLayout.setPrefSize(WIDTH, HEIGHT);
+
+    selectSuffixButton = new Button("Select suffix file");
+    selectSuffixButton.setPrefSize(WIDTH / 3, HEIGHT);
+    selectSuffixButton.setFont(font);
+    selectSuffixButton.wrapTextProperty().setValue(true);
+    selectSuffixButton.setOnAction(this::handleSelectButton);
+
+    selectDocumentButton = new Button("Select document file");
+    selectDocumentButton.setPrefSize(WIDTH / 3, HEIGHT);
+    selectDocumentButton.setFont(font);
+    selectDocumentButton.wrapTextProperty().setValue(true);
+    selectDocumentButton.setOnAction(this::handleSelectButton);
+
+    Button goButton = new Button("Go!");
+    goButton.setPrefSize(WIDTH / 3, HEIGHT);
+    goButton.setFont(font);
+    goButton.setOnAction(e -> controller.runInterleaver());
+
+    buttonLayout.getChildren().addAll(selectSuffixButton, selectDocumentButton, goButton);
+
+    rootNode.setTop(buttonLayout);
+
+    statusLabel = new Label("Select a file to begin");
+    statusLabel.setFont(font);
+    rootNode.setCenter(statusLabel);
+
+    return rootNode;
+  }
+
+  public void reportStatus(String status, boolean successful) {
+    statusLabel.setText(status);
+    statusLabel.setTextFill(successful ? Color.GREEN : Color.RED);
+  }
+
+  private void handleSelectButton(ActionEvent e) {
+    // File chooser
+    FileChooser fc = new FileChooser();
+    fc.setTitle("Image file to reveal from");
+    FileChooser.ExtensionFilter pdfFilter
+        = new ExtensionFilter("PDF Documents", "*.pdf");
+    fc.getExtensionFilters().add(pdfFilter);
+
+
+    Button source = (Button) e.getSource();
+    File chosenFile = fc.showOpenDialog(source.getScene().getWindow());
+
+    if (source == selectSuffixButton) {
+      controller.setSuffixPdf(chosenFile);
+    } else if (source == selectDocumentButton) {
+      controller.setDocumentPdf(chosenFile);
+    } else {
+      throw new UnsupportedOperationException("Unexpected Button Press");
+    }
   }
 
 }
