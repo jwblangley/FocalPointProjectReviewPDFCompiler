2a02e2b924a824e3726fe39adf5f5abfaaf3088a
James Langley <jwblangley@gmail.com>
2020-03-31 18:26:09 +0100

    Implement extracting of project manager
    Assumes that project manager column has been moved to the last column of the document

--- src/jwblangley/pdfSuffixInterleaver/filenamer/PaprikaProjectPDFNamer.java
+++ src/jwblangley/pdfSuffixInterleaver/filenamer/PaprikaProjectPDFNamer.java
@@ -1,6 +1,10 @@
 package jwblangley.pdfSuffixInterleaver.filenamer;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -10,8 +14,11 @@ public class PaprikaProjectPDFNamer implements PDFNamer {
 
   private static final String FALLBACK_NAME = "paprika-file.pdf";
 
-  private static final String PROJECT_CODE_EX = "Project Director\\s*(\\S*)/";
+  private static final String PROJECT_CODE_EX = "Project\\sManager\\s*(\\S*)/";
   private static final String DATE_EX = "Page \\d of \\d\\s+(\\d+\\s+\\S+\\s+\\d+)";
+
+  // N.B: %s will be replaced by project code
+  private static final String PROJECT_MANAGER_EX = "%s.*\\s+([A-Z]+)\\s";
 
   @Override
   public String namePDF(PDDocument document) {
@@ -20,25 +27,62 @@ public class PaprikaProjectPDFNamer implements PDFNamer {
       PDFTextStripper textStripper = new PDFTextStripper();
       String pdfContent = textStripper.getText(document);
 
-      Pattern projectCodePattern = Pattern.compile(PROJECT_CODE_EX);
-      Matcher projectCodeMatcher = projectCodePattern.matcher(pdfContent);
-
-      Pattern datePattern = Pattern.compile(DATE_EX);
-      Matcher dateMatcher = datePattern.matcher(pdfContent);
+      Matcher projectCodeMatcher = Pattern.compile(PROJECT_CODE_EX).matcher(pdfContent);
+      Matcher dateMatcher = Pattern.compile(DATE_EX).matcher(pdfContent);
 
       if (projectCodeMatcher.find() && dateMatcher.find()) {
+        // Collect results so far
         String projectCodeMatch = projectCodeMatcher.group(1);
         String dateMatch = dateMatcher.group(1);
         dateMatch = dateMatch.replaceAll(" ", "_");
 
-        return dateMatch + "-" + projectCodeMatch + ".pdf";
-      }
-      else {
+        // Find all listed project managers and choose most frequent
+        String codeInjectedProjectManagerEx = String.format(PROJECT_MANAGER_EX, projectCodeMatch);
+        Matcher projectManagerMatcher
+            = Pattern.compile(codeInjectedProjectManagerEx).matcher(pdfContent);
+
+        List<String> listedManagers = new ArrayList<>();
+        while (projectManagerMatcher.find()) {
+          listedManagers.add(projectManagerMatcher.group(1));
+        }
+
+        if (listedManagers.isEmpty()) {
+          // No match for project managers
+          return FALLBACK_NAME;
+        }
+
+        String projectManager = mode(listedManagers);
+
+        // If all succeeds - return result
+        return String.format("%s-%s-%s.pdf", projectManager, projectCodeMatch, dateMatch);
+
+      } else {
+        // No match for project code or date
         return FALLBACK_NAME;
       }
-    } catch(IOException e) {
+    } catch (IOException e) {
       e.printStackTrace();
       return FALLBACK_NAME;
     }
+  }
+
+  private <T> T mode(List<T> items) {
+    Map<T, Integer> countMap = new HashMap<>();
+
+    int max = 0;
+    T result = null;
+    for (T item : items) {
+      if (!countMap.containsKey(item)) {
+        countMap.put(item, 1);
+      } else {
+        countMap.put(item, countMap.get(item) + 1);
+      }
+
+      if (countMap.get(item) > max) {
+        max = countMap.get(item);
+        result = item;
+      }
+    }
+    return result;
   }
 }
