bf26f02d14d407cca7d439930dbbee123f5b8c2c
James Langley <jwblangley@gmail.com>
2020-04-22 23:51:18 +0100

    Implement compilePDF

--- src/jwblangley/focalPointProjectReviewPdfCompiler/model/FocalPointProjectReviewPDFCompiler.java
+++ src/jwblangley/focalPointProjectReviewPdfCompiler/model/FocalPointProjectReviewPDFCompiler.java
@@ -2,13 +2,12 @@ package jwblangley.focalPointProjectReviewPdfCompiler.model;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import jwblangley.focalPointProjectReviewPdfCompiler.filenamer.NoOverwritePDFNamer;
 import jwblangley.focalPointProjectReviewPdfCompiler.filenamer.PDFNamer;
-import jwblangley.focalPointProjectReviewPdfCompiler.filenamer.FocalPointProjectReviewPDFNamer;
+import jwblangley.focalPointProjectReviewPdfCompiler.filenamer.StringPDFNamer;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -28,8 +27,6 @@ public class FocalPointProjectReviewPDFCompiler {
     PDDocument projectReviewPage = PDDocument.load(projectReviewPageFile);
 
     Splitter splitter = new Splitter();
-    PDFNamer pdfNamer = new NoOverwritePDFNamer(new FocalPointProjectReviewPDFNamer(), outputDirectory);
-
     List<PDDocument> pages = splitter.split(focalPointDocument);
 
     Queue<PDDocument> pageQueue = new LinkedList<>();
@@ -38,7 +35,7 @@ public class FocalPointProjectReviewPDFCompiler {
       pageQueue.add(page);
 
       if (endOfSectionAt(page)) {
-        compileSection(pageQueue, projectReviewPage);
+        compileSection(pageQueue, projectReviewPage, outputDirectory);
         assert pageQueue.isEmpty(): "compileSection must consume all pages on the page queue";
       }
     }
@@ -55,20 +52,43 @@ public class FocalPointProjectReviewPDFCompiler {
     return false;
   }
 
-  private static void compileSection(Queue<PDDocument> pageQueue, PDDocument projectReviewPage) {
+  private static void compileSection(
+      Queue<PDDocument> pageQueue,
+      PDDocument projectReviewPage,
+      File outputDirectory) throws IOException {
+
+    PDFMergerUtility merger = new PDFMergerUtility();
+    PDDocument resultDoc = new PDDocument();
+
+    // First page will always be existing project review page TODO: check
+    PDDocument frontPage = pageQueue.poll();
+    // Extract information from page
+
+    // Replace first page
+    PDDocument filledPVPage = fillProjectReviewPage(projectReviewPage);
+    merger.appendDocument(resultDoc, filledPVPage);
+    filledPVPage.close();
+    frontPage.close();
+
+    // Read from pageQueue
+    while (!pageQueue.isEmpty()) {
+      PDDocument page = pageQueue.poll();
+      merger.appendDocument(resultDoc, page);
+      page.close();
+    }
+
+    String resultName = "";
+    PDFNamer pdfNamer = new NoOverwritePDFNamer(new StringPDFNamer(resultName), outputDirectory);
+
+    File resultFile = new File(outputDirectory, pdfNamer.namePDF(resultDoc));
+
+    resultDoc.save(resultFile.getPath());
+    resultDoc.close();
+  }
+
+  private static PDDocument fillProjectReviewPage(PDDocument projectReviewPage) {
     // TODO
-//    PDFMergerUtility merger = new PDFMergerUtility();
-//
-//    PDDocument resultDoc = new PDDocument();
-//
-//    merger.appendDocument(resultDoc, page);
-//    merger.appendDocument(resultDoc, suffixPage);
-//
-//    File resultFile = new File(outputDirectory, pdfNamer.namePDF(resultDoc));
-//
-//    resultDoc.save(resultFile.getPath());
-//    resultDoc.close();
-//    page.close();
+    return null;
   }
 
 
